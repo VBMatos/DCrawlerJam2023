5185429eca30ef223421f7223ee61fa892a24cc3
Vitor Batista Matos <vitorbatistamatos@gmail.com>
2023-02-21 15:43:49 -0300

    3D test

--- core/src/com/mygdx/game/DCrawlerJam.java
+++ core/src/com/mygdx/game/DCrawlerJam.java
@@ -1,31 +1,75 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class DCrawlerJam extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private PerspectiveCamera camera;
+	private CameraInputController cameraController;
+	private Environment environment;
+	private ModelBatch modelBatch;
+	private Model model;
+	private ModelInstance instance;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+		modelBatch = new ModelBatch();
+
+		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(10f, 10f, 10f);
+		camera.lookAt(0,0,0);
+		camera.near = 1f;
+		camera.far = 300f;
+		camera.update();
+
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(5f, 5f, 5f, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Position | Usage.Normal);
+		instance = new ModelInstance(model);
+
+		cameraController = new CameraInputController(camera);
+		Gdx.input.setInputProcessor(cameraController);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		camera.update();
+
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(camera);
+		modelBatch.render(instance, environment);
+		modelBatch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		modelBatch.dispose();
+		model.dispose();
+	}
+
+	@Override
+	public void resume () {
+	}
+
+	@Override
+	public void resize (int width, int height) {
+	}
+
+	@Override
+	public void pause () {
 	}
 }
