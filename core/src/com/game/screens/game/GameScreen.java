7cdc1ee602adbe2c33fc922082034a2afc2c9b41
Vitor Batista Matos <vitorbatistamatos@gmail.com>
2023-02-24 09:20:02 -0300

    Import dungeon map from Blender

--- core/src/com/game/screens/game/GameScreen.java
+++ core/src/com/game/screens/game/GameScreen.java
@@ -2,6 +2,7 @@ package com.game.screens.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -12,12 +13,18 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
 import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.JsonReader;
 import com.game.DCrawlerJam;
 
-public class GameScreen implements Screen {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GameScreen implements Screen {
     private final DCrawlerJam game;
 
     //Fixed resolution
@@ -29,7 +36,10 @@ public class GameScreen implements Screen {
     private Environment environment;
     private ModelBatch modelBatch;
     private Model model;
-    private ModelInstance instance;
+    private AssetManager assets;
+
+    private List<ModelInstance> lsInstances = new ArrayList<>();
+    private boolean loading;
 
     public SpriteBatch batch;
 
@@ -49,9 +59,9 @@ public class GameScreen implements Screen {
         camera.far = 300f;
         camera.update();
 
-        ModelLoader loader = new ObjLoader();
-        model = loader.loadModel(Gdx.files.internal("models/sphere.obj"));
-        instance = new ModelInstance(model);
+        assets = new AssetManager();
+        assets.load("models/dcrawler/dcrawler.g3db", Model.class);
+        loading = true;
 
         cameraController = new CameraInputController(camera);
         Gdx.input.setInputProcessor(cameraController);
@@ -62,15 +72,25 @@ public class GameScreen implements Screen {
 
     }
 
+    private void doneLoading(){
+        Model dungeonModel = assets.get("models/dcrawler/dcrawler.g3db", Model.class);
+        ModelInstance instance = new ModelInstance(dungeonModel);
+        lsInstances.add(instance);
+        loading = false;
+    }
+
     @Override
     public void render(float delta) {
+        if (loading && assets.update())
+            doneLoading();
+
         camera.update();
 
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         modelBatch.begin(camera);
-        modelBatch.render(instance, environment);
+        modelBatch.render(lsInstances, environment);
         modelBatch.end();
     }
 
